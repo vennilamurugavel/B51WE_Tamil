9eb089e0786dc11041299b79f123d0d5d7c8f527
vennilamurugavel <vennilageo@gmail.com>
2023-09-09 10:56:07 +0530

    resume

---
+++ day4_task/resume_Json.js
@@ -0,0 +1,39 @@
+let vennila_resume={
+"Details":{
+    "myname":"Vennila",
+    "Email-id":"vennilageo@gmail.com",
+    "mobilenumber":9003537003,
+    "location":{
+        "H.no":"7/37A",
+        "street":"west street",
+        "village":"M.Maniyakaranpatty",
+"district":"Dindigul",
+"state":"TamilNadu",
+"Postalcode":624306
+    },
+    "work Experience":{
+        "Companyname":"Wipro",
+        "year of experience":4,
+        "location":"Bangalore",
+        "Process_Explanation":"Working in lidar mapping project. find object and create label using real time resources.",
+
+    },
+    "Educational Information":
+        [{
+            "Degree":"Ms.c Geoinformatics",
+            "year of passed":"2019",
+            "college name":"gandhigram rural university"
+        }],
+       "UG": [{
+            "Degree":"B.Sc Mathematics",
+            "year of passed":"2017",
+            "college name":"gandhigram rural university"
+        }],
+        "skills":
+        [{"Languages and tools known":"ArcGis,Erdas,Google earth pro,Javascript,Html,Css"}],
+        "Languages":"Tamil,English",
+        "interest":"Coding,Problem solving",
+    }
+}
+
+console.log(vennila_resume);
